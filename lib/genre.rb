c237c305598f4013351db01292c1ec9880ecdcb5
Grace Kim <goooyoon@gmail.com>
2020-06-09 17:22:44 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,6 +6,17 @@ def initialize(name)
   @name = name
 end
 
-def songs
+  def songs
+    Song.all.select {|song| song.genre ==self }
+  end
   
+  def self.all
+    @@all
+  end
+  
+  def artists
+    array = Song.all.select {|song| song.genre == self}
+    array.map {|song| song.artist}
+  end
+
 end
