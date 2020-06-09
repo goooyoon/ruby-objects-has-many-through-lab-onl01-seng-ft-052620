bb0648da7ed74ae4c23033aa5c2ea496e76a3f88
Grace Kim <goooyoon@gmail.com>
2020-06-09 17:49:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,19 +1,28 @@
 class Doctor
+  
   attr_reader :name
   
   @@all =[]
   
   def initialize(name)
-    @name = name
+    @name=name
     @@all << self
+  end
+  
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    self.appointments.map {|appt| appt.patient}
   end
   
   def self.all
     @@all
   end
   
-  def appointment
-    Appointment.all.select {|appointment| appointment.doctor == self}
-end
-
 end
