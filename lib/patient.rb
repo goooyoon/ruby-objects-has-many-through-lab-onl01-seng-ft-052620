4207912152929bb715f6b3ec44573e6fc5a55166
Grace Kim <goooyoon@gmail.com>
2020-06-09 17:42:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,22 @@
 class Patient
   
   attr_reader :name
+  
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
     end
+    
+def self.all
+  @@all
 end
+
+def appointments
+  
+end
+
+end
+
+
